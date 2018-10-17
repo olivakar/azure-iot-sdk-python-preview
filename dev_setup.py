599d246a15f353178a098b9035ec6e4004f5dbc2
Carter Tinney <catinney@microsoft.com>
2018-10-16 17:40:38 -0700

    removed duplicate license header

--- dev_setup.py
+++ dev_setup.py
@@ -5,10 +5,6 @@
 
 """Prepare development environment
 """
-# --------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
 
 import glob
 import os
