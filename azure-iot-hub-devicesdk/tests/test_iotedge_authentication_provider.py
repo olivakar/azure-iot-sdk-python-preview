6d6c3a64ad86fc8bf1354221e20fe4c99160f6b7
Bert Kleewein <bertk@microsoft.com>
2018-10-23 13:49:05 -0700

    add tests for environment and group tests into objects

--- azure-iot-hub-devicesdk/tests/test_iotedge_authentication_provider.py
+++ azure-iot-hub-devicesdk/tests/test_iotedge_authentication_provider.py
@@ -4,38 +4,53 @@
 # --------------------------------------------------------------------------------------------
 import pytest
 
-def test_iotedge_auth_provider_calls_into_edge_daemon_to_sign_for_connection_string():
-    """
-    Verify that the iotedge authentication provider calls into the edge daemon using
-    HTTP with a propertly constructed REST calls in order to do a signing operation.
-    """
-    pass
+class TestSignatureOperation(object):
+    def signing_operation_fails_if_environment_variables_are_missing(self):
+        """
+        verify that signature operations fail if the needed environment variables are missing.
+        """
+        pass
 
-def test_iotedge_auth_provider_fails_if_http_signing_operation_fails():
-    """
-    Verify that the iotedge authentication provider fails if the http call for the
-    signing operation fails.
-    """
-    pass
+    def signing_operation_calls_into_edge_daemon_to_sign_for_connection_string(self):
+        """
+        Verify that the iotedge authentication provider calls into the edge daemon using
+        HTTP with a propertly constructed REST calls in order to do a signing operation.
+        """
+        pass
 
-def test_iotedge_auth_provider_signature_creates_valid_sas_token():
-    """
-    Verify that the result of the signing operation is used to create a valid
-    SAS token.
-    """
-    pass
+    def signing_operation_fails_if_http_signing_operation_fails(self):
+        """
+        Verify that the iotedge authentication provider fails if the http call for the
+        signing operation fails.
+        """
+        pass
 
-def test_iotedge_auth_provider_returns_correct_ca_from_iotedge_trust_bundle():
-    """
-    Verify that the iotedge authentication provider calls into the edge daemon using
-    HTTP with a propertly constructed REST call in order to retrieve and return the
-    trust bundle.
-    """
-    pass
+    def signing_operation_creates_valid_sas_token(self):
+        """
+        Verify that the result of the signing operation is used to create a valid
+        SAS token.
+        """
+        pass
 
-def test_iotedge_auth_provider_fails_if_http_call_to_get_trust_bundle_fails():
-    """
-    Verify that an HTTP failure getting the trust bundle will cause the call to
-    get_trusted_certificate_authority to fail.
-    """
-    pass
+class TestGetCaCert(object):
+    def get_fails_if_environment_variables_are_missing(self):
+        """
+        verify that signature operations fail if the needed environment variables are missing.
+        """
+        pass
+
+
+    def get_returns_correct_ca_from_iotedge_trust_bundle(self):
+        """
+        Verify that the iotedge authentication provider calls into the edge daemon using
+        HTTP with a propertly constructed REST call in order to retrieve and return the
+        trust bundle.
+        """
+        pass
+
+    def get_fails_if_http_call_to_get_trust_bundle_fails(self):
+        """
+        Verify that an HTTP failure getting the trust bundle will cause the call to
+        get_trusted_certificate_authority to fail.
+        """
+        pass
